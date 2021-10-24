667e43a8cc34689b57884740b488979cb2ab05a6
Bastien Germond <bastien.germond@epita.fr>
2021-10-24 18:41:29 +0200

    networking: add nameservers

--- configuration.nix
+++ configuration.nix
@@ -38,10 +38,8 @@
   # Set your time zone.
   time.timeZone = "Europe/Paris";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+
   networking.interfaces.wlp0s20f3.useDHCP = true;
 
   networking.networkmanager.enable = true;
