52d9bb04a9bf2fc44002c9723efe1512325830bb
Bastien Germond <bastien.germond@epita.fr>
2022-03-27 21:29:33 +0200

    hosts: coral: add hardware configuration

--- hosts/coral/hardware-configuration.nix
+++ hosts/coral/hardware-configuration.nix
@@ -1,28 +1,12 @@
 { config, pkgs, modulesPath, lib, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  boot.cleanTmpDir = true;
+  zramSwap.enable = true;
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/3fbd1a44-509a-472e-9158-72c217285549";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/4650-3B2F";
-      fsType = "vfat";
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/5dadd3ca-42d1-4de5-a1c1-63a507affa91"; }
-    ];
-
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  boot.loader.grub.device = "/dev/sda";
+  boot.initrd.kernelModules = [ "nvme" ];
+  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
 }
