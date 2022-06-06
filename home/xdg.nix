e8b7ba5bc4a800c2124464216373727aa889ffb6
Bastien Germond <bastien.germond@epita.fr>
2022-06-06 15:36:22 +0200

    fix: configure xfg to use librewolf

--- home/xdg.nix
+++ home/xdg.nix
@@ -1,7 +1,28 @@
-{config, pkgs, inputs, ...}:
+{ config, pkgs, inputs, ... }:
 
 {
   xdg.enable = true;
+
+  xdg.mime.enable = true;
+  xdg.mimeApps = {
+    enable = true;
+    associations.added = {
+      "application/pdf" = [ "evince.desktop" ];
+      "text/html" = [ "librewolf.desktop" ];
+      "x-scheme-handler/http" = [ "librewolf.desktop" ];
+      "x-scheme-handler/https" = [ "librewolf.desktop" ];
+      "x-scheme-handler/about" = [ "librewolf.desktop" ];
+      "x-scheme-handler/unknown" = [ "librewolf.desktop" ];
+    };
+    defaultApplications = {
+      "application/pdf" = "evince.desktop";
+      "text/html" = [ "librewolf.desktop" ];
+      "x-scheme-handler/http" = [ "librewolf.desktop" ];
+      "x-scheme-handler/https" = [ "librewolf.desktop" ];
+      "x-scheme-handler/about" = [ "librewolf.desktop" ];
+      "x-scheme-handler/unknown" = [ "librewolf.desktop" ];
+    };
+  };
 
   xdg.userDirs = {
     download = "\$HOME/Downloads";
