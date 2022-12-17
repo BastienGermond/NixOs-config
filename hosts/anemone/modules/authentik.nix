089fb33754152755dce13c3e3186317784d97433
Bastien Germond <bastien.germond@epita.fr>
2022-12-17 17:40:47 +0100

    hosts(anemone): bump authentik to 2022.11.2

--- hosts/anemone/modules/authentik.nix
+++ hosts/anemone/modules/authentik.nix
@@ -4,7 +4,7 @@
   services.authentik = {
     enable = true;
     # image = "ghcr.io/goauthentik/dev-server";
-    version = "2022.11.1";
+    version = "2022.11.2";
     authentikServerEnvironmentFiles = [
       config.sops.secrets.authentik.path
     ];
