fa492d0b0e6bc222cffa796cef1c2e6ead52f9a1
Bastien Germond <bastien.germond@epita.fr>
2023-02-11 20:45:00 +0100

    refactor: move all wireguard configuration in infra.nix

--- hosts/coral/modules/wireguard.nix
+++ hosts/coral/modules/wireguard.nix
@@ -1,38 +1,22 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, infra, ... }:
 
 {
   networking.nat.enable = true;
   networking.nat.externalInterface = "eth0";
-  networking.nat.internalInterfaces = [ "wgs0" ];
+  networking.nat.internalInterfaces = builtins.attrNames infra.hosts.coral.wireguard;
 
-  networking.wireguard.interfaces = {
-    wgs0 = {
-      ips = [ "10.100.10.1/24" ];
-      listenPort = 51821;
-      privateKeyFile = "/root/.wg/wgs0.pkey";
+  networking.wireguard.interfaces = lib.mkMerge [
+    infra.hosts.coral.wireguard
+    {
+      wgs0 = {
+        postSetup = ''
+          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.10.0/24 -o eth0 -j MASQUERADE
+        '';
 
-      postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.10.0/24 -o eth0 -j MASQUERADE
-      '';
-
-      postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.10.0/24 -o eth0 -j MASQUERADE
-      '';
-
-      peers = [
-        {
-          publicKey = "rQukuG5PzfSBX4Q28ubHVWGIN+e75Mm8NzPaK8y601A=";
-          allowedIPs = [ "10.100.10.18/32" ];
-        }
-        {
-          publicKey = "ptVPzlnSRGpOVrlD/cYhvG/AKEAWe32UaDPAo0ivnG4=";
-          allowedIPs = [ "10.100.10.2/32" ];
-        }
-        {
-          publicKey = "hHWuxL+GuQ78uZNcUqTqyTdsCNDXXlsMlxf6IFm5ZUM=";
-          allowedIPs = [ "10.100.10.3/32" ];
-        }
-      ];
-    };
-  };
+        postShutdown = ''
+          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.10.0/24 -o eth0 -j MASQUERADE
+        '';
+      };
+    }
+  ];
 }
---
+++ hosts/infra.nix
@@ -0,0 +1,84 @@
+rec {
+  hosts = {
+    coral = {
+      ips = {
+        public = {
+          A = "135.181.36.15";
+          AAAA = "";
+        };
+        vpn = {
+          A = "10.100.10.1";
+          AAAA = "";
+        };
+      };
+      wireguard = {
+        wgs0 = {
+          ips = [ "${hosts.coral.ips.vpn.A}/24" ];
+          listenPort = 51821;
+          privateKeyFile = "/root/.wg/wgs0.pkey";
+
+          peers = [
+            {
+              publicKey = "ptVPzlnSRGpOVrlD/cYhvG/AKEAWe32UaDPAo0ivnG4=";
+              allowedIPs = [ hosts.anemone.ips.vpn.A ];
+            }
+            {
+              publicKey = "hHWuxL+GuQ78uZNcUqTqyTdsCNDXXlsMlxf6IFm5ZUM=";
+              allowedIPs = [ hosts.synapze-pc.ips.vpn.A ];
+            }
+          ];
+        };
+      };
+    };
+
+    anemone = {
+      ips = {
+        vpn = {
+          A = "10.100.10.2";
+          AAAA = "";
+        };
+      };
+      wireguard = {
+        wg0 = {
+          ips = [ "${hosts.anemone.ips.vpn.A}/32" ];
+          listenPort = 51821;
+          privateKeyFile = "/root/.wg/wg0.pkey";
+
+          peers = [
+            {
+              publicKey = "IOXJd4A9NO9JMcRcQRl5QYL8WW0s13+PMnyZVbbr728=";
+              allowedIPs = [ "10.100.10.0/24" ];
+              endpoint = "${hosts.coral.ips.public.A}:${builtins.toString hosts.coral.wireguard.wgs0.listenPort}";
+              persistentKeepalive = 25;
+            }
+          ];
+        };
+      };
+    };
+
+    synapze-pc = {
+      ips = {
+        vpn = {
+          A = "10.100.10.3";
+          AAAA = "";
+        };
+      };
+      wireguard = {
+        wg0 = {
+          ips = [ "${hosts.synapze-pc.ips.vpn.A}/32" ];
+          listenPort = 51821;
+          privateKeyFile = "/home/synapze/.wg/wg0.pkey";
+
+          peers = [
+            {
+              publicKey = "IOXJd4A9NO9JMcRcQRl5QYL8WW0s13+PMnyZVbbr728=";
+              allowedIPs = [ "10.100.10.0/24" ];
+              endpoint = "${hosts.coral.ips.public.A}:${builtins.toString hosts.coral.wireguard.wgs0.listenPort}";
+              persistentKeepalive = 25;
+            }
+          ];
+        };
+      };
+    };
+  };
+}
