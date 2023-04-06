91b4ad420dc017612150c6d85ff45f941bd06511
Bastien Germond <bastien.germond@epita.fr>
2023-04-06 13:22:32 +0200

    bump gatus 5.3.1 -> 5.3.2

--- pkgs/gatus/default.nix
+++ pkgs/gatus/default.nix
@@ -2,16 +2,16 @@
 
 pkgs.buildGoModule rec {
   pname = "gatus";
-  version = "5.3.1";
+  version = "5.3.2";
 
   src = pkgs.fetchFromGitHub {
     owner = "TwiN";
     repo = "gatus";
     rev = "v${version}";
-    sha256 = "sha256-64H1Y3a1RJbo+tX2SBePwHWCEvQXnwyqgowcnOvKreQ=";
+    sha256 = "sha256-0y6KHvYexZiBJX1Z+zQtvPzsZbLc0SAk1xirZFBGJSk=";
   };
 
-  vendorSha256 = "sha256-ziKdlOFOddh/CBqYsr74iMpIelE8Idv+fA2aSe5h+X8=";
+  vendorSha256 = "sha256-ITkKaNlY3Q7A+7sMcSoyyVqiXgnVotfM/ZAn6QcUGII=";
 
   doCheck = false;
 }
