6a2c1d9a1f160fa195e949a7a229f9a48fbb033f
Bastien Germond <bastien.germond@epita.fr>
2023-08-12 12:57:49 +0200

    nix: add helix cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -8,6 +8,12 @@
     settings = {
       auto-optimise-store = true;
       trusted-users = ["synapze"];
+      substituters = [
+        "https://helix.cachix.org"
+      ];
+      trusted-public-keys = [
+        "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      ];
     };
     extraOptions = ''
       experimental-features = nix-command flakes
