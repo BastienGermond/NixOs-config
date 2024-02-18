dd8b0abb59a095ceb5eeb0ebca0db14f4985fd8a
Bastien Germond <bastien.germond@epita.fr>
2024-02-18 11:27:19 +0100

    fix: ssh authorized keys user-writeable file result in an insecure configuration

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,16 +1,28 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
-  security.pam.sshAgentAuth.enable = true;
+  security.pam.sshAgentAuth = {
+    enable = true;
+    authorizedKeysFiles = [
+      "/etc/ssh/authorized_keys.d/%u"
+      # "%h/.ssh/authorized_keys" INSECURE
+    ];
+  };
 
   services.openssh = {
     enable = true;
+    authorizedKeysFiles = lib.mkForce ["/etc/ssh/authorized_keys.d/%u"];
     settings = {
       PasswordAuthentication = false;
       PermitRootLogin = "no";
       X11Forwarding = true;
     };
   };
+
+  users.users.synapze.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJh2B4ZYF7UfJ//s1kK+uaSDYKfvcO94JMpk3VHLJY3h synapze@synapze-pc"
+  ];
 }
