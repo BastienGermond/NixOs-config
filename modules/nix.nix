435b54f2910b7dd0c2ab4e7e2e7fb428c13df6fd
Bastien Germond <bastien.germond@epita.fr>
2024-03-01 20:16:31 +0100

    nix: add unfree cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -10,9 +10,11 @@
       trusted-users = ["synapze"];
       substituters = [
         "https://helix.cachix.org"
+        "https://numtide.cachix.org"
       ];
       trusted-public-keys = [
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       ];
     };
     extraOptions = ''
