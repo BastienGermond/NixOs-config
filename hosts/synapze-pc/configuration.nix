c36baeddf1b07721a063a83d9face162afc2640a
Bastien Germond <bastien.germond@epita.fr>
2024-07-28 11:49:28 +0200

    fix formatting

--- hosts/synapze-pc/configuration.nix
+++ hosts/synapze-pc/configuration.nix
@@ -18,7 +18,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.tmp.cleanOnBoot = true;
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  boot.binfmt.emulatedSystems = ["aarch64-linux"];
 
   environment.variables.XDG_CONFIG_HOME = "$HOME/.config";
   environment.variables.TERM = "alacritty";
