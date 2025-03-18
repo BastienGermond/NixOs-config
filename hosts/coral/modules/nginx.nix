aba2bba770ba0d3f9f3c57d7d1820bf91743c353
Bastien Germond <bastien.germond@epita.fr>
2025-03-18 16:06:17 +0100

    coral: use callPackage pattern to import hosts

--- hosts/coral/modules/nginx.nix
+++ hosts/coral/modules/nginx.nix
@@ -54,6 +54,11 @@ in {
 
           conf
         ];
+
+      importHost = host: overrides: let
+        f = import host;
+      in
+        f ((builtins.intersectAttrs (builtins.functionArgs f) (pkgs // {inherit config anemone coral withDefaultConfiguration;})) // overrides);
     in {
       # be the default to trap all the direct ip access.
       # TODO: fail2ban almost instantly those ips
@@ -66,27 +71,26 @@ in {
         locations."/".return = "444"; # 444 with nginx close connection without response.
       };
 
-      # "gistre.fr" = import ./nginx/gistre.fr.nix {inherit pkgs;};
-      "germond.org" = import ./nginx/germond.org.nix {inherit anemone withDefaultConfiguration;};
-      "newsso.germond.org" = import ./nginx/newsso.germond.org.nix {inherit coral withDefaultConfiguration;};
-      "cloud.germond.org" = import ./nginx/cloud.germond.org.nix {inherit anemone withDefaultConfiguration;};
-      "grafana.germond.org" = import ./nginx/grafana.germond.org.nix {inherit config withDefaultConfiguration;};
-      "status.germond.org" = import ./nginx/status.germond.org.nix {inherit config withDefaultConfiguration;};
-      "minio.germond.org" = import ./nginx/minio.germond.org.nix {inherit anemone withDefaultConfiguration;};
-      "s3.germond.org" = import ./nginx/s3.germond.org.nix {inherit anemone withDefaultConfiguration;};
-      "t.germond.org" = import ./nginx/t.germond.org.nix {inherit config withDefaultConfiguration;};
-      "paperless.germond.org" = import ./nginx/paperless.germond.org.nix {inherit anemone withDefaultConfiguration;};
-      "hackmd.germond.org" = import ./nginx/hackmd.germond.org.nix {inherit config withDefaultConfiguration;};
-      "git.germond.org" = import ./nginx/git.germond.org.nix {inherit anemone withDefaultConfiguration;};
-      "docs.germond.org" = import ./nginx/docs.germond.org.nix {inherit anemone withDefaultConfiguration;};
-      "videos.germond.org" = import ./nginx/videos.germond.org.nix {inherit anemone withDefaultConfiguration;};
-      "vouch.germond.org" = import ./nginx/vouch.germond.org.nix {inherit withDefaultConfiguration;};
-      "prometheus.germond.org" = import ./nginx/prometheus.germond.org.nix {inherit pkgs lib config withDefaultConfiguration;};
-      "home.germond.org" = import ./nginx/home.germond.org.nix {inherit coral withDefaultConfiguration;};
-      "alert.germond.org" = import ./nginx/alert.germond.org.nix {inherit coral withDefaultConfiguration;};
-      "scrutiny.germond.org" = import ./nginx/scrutiny.germond.org.nix {inherit config withDefaultConfiguration;};
-      "cache.germond.org" = import ./nginx/cache.germond.org.nix {inherit coral withDefaultConfiguration;};
-      "immich.germond.org" = import ./nginx/immich.germond.org.nix {inherit anemone withDefaultConfiguration;};
+      "germond.org" = importHost ./nginx/germond.org.nix {};
+      "newsso.germond.org" = importHost ./nginx/newsso.germond.org.nix {};
+      "cloud.germond.org" = importHost ./nginx/cloud.germond.org.nix {};
+      "grafana.germond.org" = importHost ./nginx/grafana.germond.org.nix {};
+      "status.germond.org" = importHost ./nginx/status.germond.org.nix {};
+      "minio.germond.org" = importHost ./nginx/minio.germond.org.nix {};
+      "s3.germond.org" = importHost ./nginx/s3.germond.org.nix {};
+      "t.germond.org" = importHost ./nginx/t.germond.org.nix {};
+      "paperless.germond.org" = importHost ./nginx/paperless.germond.org.nix {};
+      "hackmd.germond.org" = importHost ./nginx/hackmd.germond.org.nix {};
+      "git.germond.org" = importHost ./nginx/git.germond.org.nix {};
+      "docs.germond.org" = importHost ./nginx/docs.germond.org.nix {};
+      "videos.germond.org" = importHost ./nginx/videos.germond.org.nix {};
+      "vouch.germond.org" = importHost ./nginx/vouch.germond.org.nix {};
+      "prometheus.germond.org" = importHost ./nginx/prometheus.germond.org.nix {};
+      "home.germond.org" = importHost ./nginx/home.germond.org.nix {};
+      "alert.germond.org" = importHost ./nginx/alert.germond.org.nix {};
+      "scrutiny.germond.org" = importHost ./nginx/scrutiny.germond.org.nix {};
+      "cache.germond.org" = importHost ./nginx/cache.germond.org.nix {};
+      "immich.germond.org" = importHost ./nginx/immich.germond.org.nix {};
     };
   };
 
