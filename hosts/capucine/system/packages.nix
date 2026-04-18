91159875cefb52befbc335a0eb25513f3f6aec0f
Bastien Germond <bastien.germond@epita.fr>
2026-04-18 18:21:44 +0200

    capucine: add font routed-gothic

--- hosts/capucine/system/packages.nix
+++ hosts/capucine/system/packages.nix
@@ -1,37 +1,45 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: let
+  routed-gothic = pkgs.stdenv.mkDerivation {
+    pname = "routed-gothic";
+    version = "1.0.0";
+
+    src = pkgs.fetchzip {
+      url = "https://github.com/dse/routed-gothic/raw/4f90a75bb7b388006a63d2d63d7c55b0509acdaf/download/routed-gothic-ttf-v1.0.0.zip";
+      hash = "sha256-R7EaYsi1Q854qoOgXOl0RWohSAkLfYcriCzaeh9WgXo=";
+    };
+
+    installPhase = ''
+      mkdir -p $out/share/fonts/truetype
+      cp *.ttf $out/share/fonts/truetype/
+    '';
+  };
+in {
   fonts.packages = with pkgs; [
     fira
     font-awesome
     julia-mono
-    noto-fonts
-    roboto
-    termsyn
-    # (nerdfonts.override {fonts = ["FiraMono" "FiraCode" "Terminus"];})
-    nerd-fonts.fira-mono
-    nerd-fonts.fira-code
-    terminus_font
     liberation_ttf
+    nerd-fonts.fira-code
+    nerd-fonts.fira-mono
+    # (nerdfonts.override {fonts = ["FiraMono" "FiraCode" "Terminus"];})
+    noto-fonts
+    overpass
+    roboto
+    routed-gothic
+    terminus_font
+    termsyn
   ];
 
   environment.systemPackages = let
     inherit (pkgs) makeDesktopItem;
 
     lycheeslicer = pkgs.lycheeslicer;
-    
     # pkgs.stdenv.mkDerivation (finalAttrs: {
     #   inherit (pkgs.LycheeSlicer) pname version meta;
-
     #   buildInputs = [pkgs.makeWrapper];
     #   nativeBuildInputs = [pkgs.makeWrapper];
     #   propagatedBuildInputs = [pkgs.xorg.libxshmfence];
-
     #   unpackPhase = "true"; # No source to unpack
-
     #   desktopItem = makeDesktopItem {
     #     name = "LycheeSlicer";
     #     exec = "LycheeSlicer";
@@ -39,12 +47,9 @@
     #     desktopName = "Lychee Slicer";
     #     categories = ["Graphics"];
     #   };
-
     #   installPhase = ''
     #     mkdir -p $out/{bin,share}
-
     #     install -Dm644 "$desktopItem/share/applications/LycheeSlicer.desktop" "$out/share/applications/LycheeSlicer.desktop"
-
     #     makeWrapper ${pkgs.LycheeSlicer}/bin/LycheeSlicer $out/bin/LycheeSlicer \
     #       --set LD_LIBRARY_PATH ${pkgs.xorg.libxshmfence}/lib
     #   '';
